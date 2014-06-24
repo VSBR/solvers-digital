0dd1678a4c5c05ff6884d1b6cba796e248870099
田島 寛之 <tashima@000150-m2.local>
2014-06-24 19:38:32 +0900

    main sceneでstart段階の遷移させるところまででーきた

--- js/scene/main.js
+++ js/scene/main.js
@@ -27,31 +27,51 @@ appSolvers.getScene().main = {
             //affairs
             var AFFAIRS_MAIN = 3;
             var AFFAIRS_BUILD = 4;
-            var AFFAIRS_HERO_MAKE = 5;
-            var AFFAIRS_HERO_UP = 6;
+            var AFFAIRS_CREATE_HERO = 5;
+            var AFFAIRS_UP_HERO = 6;
             var AFFAIRS_WEAPON = 7;
             //game result
             var GAME_CLEAR = 8;
             var GAME_RESULT = 9;
             /* * * * * * * * * * */
+
             //game controll
             self.scene = ko.observable();
+
+            //ui elements
+            self.uiDisplayBlock = $("")
+
+            //game data init
+
+            //ui init
+
+
+
+            //scene method
+            self.mainScene = _.bind(function (data) {
+                console.log("[scene] start");
+
+
+            });
+
 
             self.model = model;
             // Modelの変化を監視
             self.model.data.subscribe(function (data) {
             });
 
-            //sceneの状態を監視
+
+            //sceneの遷移
             self.scene
-                .subscribe(_.bind(function () {
-                    var _scene = this.scene();
+                .subscribe( function (data) {
+                    var _scene = self.scene();
+                    console.log("scene : " + _scene);
                     switch(_scene) {
-                        case START:
-                            console.log("scene : " + _scene);
+                        case "START":
+                            self.mainScene();
                         break;
                     }
-                }));
+                });
         };
 
         appSolvers.getViewModel().main = {};
@@ -60,8 +80,11 @@ appSolvers.getScene().main = {
         var mainViewModel = new MainViewModel(model);
         appSolvers.getScene().main = mainViewModel;
 
+        //init
+        
+
         console.log("main start");
-//        this.scene(this.START);
+        appSolvers.getScene().main.scene("START");
 
     }
 };
